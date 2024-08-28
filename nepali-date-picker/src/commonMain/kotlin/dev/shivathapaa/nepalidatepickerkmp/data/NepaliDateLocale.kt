3c4c38f1d242f258e3a4938a3e64c9a6ee5975e9
Shiva Thapa <shiva@pathway.com.np>
2024-08-28 21:52:35 +0545

    Change Locale defaults

--- nepali-date-picker/src/commonMain/kotlin/dev/shivathapaa/nepalidatepickerkmp/data/NepaliDateLocale.kt
+++ nepali-date-picker/src/commonMain/kotlin/dev/shivathapaa/nepalidatepickerkmp/data/NepaliDateLocale.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright © 2024 Shiva Thapa (@shivathapaa). All rights reserved.
+ *
+ * Licensed under the Mozilla Public License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://mozilla.org/MPL/2.0/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package dev.shivathapaa.nepalidatepickerkmp.data
 
 import androidx.compose.runtime.Immutable
@@ -5,7 +21,7 @@ import androidx.compose.runtime.Immutable
 @Immutable
 data class NepaliDateLocale(
     val language: NepaliDatePickerLang = NepaliDatePickerLang.ENGLISH,
-    val dateFormat: NepaliDateFormatStyle = NepaliDateFormatStyle.MEDIUM,
+    val dateFormat: NepaliDateFormatStyle = NepaliDateFormatStyle.LONG,
     val weekDayName: NameFormat = NameFormat.FULL,
     val monthName: NameFormat = NameFormat.FULL
 )
