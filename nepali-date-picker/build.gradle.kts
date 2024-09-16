92bc494016b53480a1d9f15f86dafc0539985bde
Shiva Thapa <shivathapa0600@gmail.com>
2024-09-16 23:24:05 +0545

    Release of version 2.1.0-alpha01 with English Date in Picker

--- nepali-date-picker/build.gradle.kts
+++ nepali-date-picker/build.gradle.kts
@@ -81,7 +81,7 @@ mavenPublishing {
     coordinates(
         groupId = "io.github.shivathapaa",
         artifactId = "nepali-date-picker",
-        version = "2.0.0-rc01"
+        version = "2.1.0-alpha01"
     )
 
     // POM metadata for the published artifact
