78af37ccb1b85ad16322f14766d28347ccd14522
Shiva Thapa <shivathapa0600@gmail.com>
2024-10-01 00:00:46 +0545

    Release of version 2.1.0-alpha03 with Nepali Date Ranger Picker support

--- nepali-date-picker/build.gradle.kts
+++ nepali-date-picker/build.gradle.kts
@@ -81,7 +81,7 @@ mavenPublishing {
     coordinates(
         groupId = "io.github.shivathapaa",
         artifactId = "nepali-date-picker",
-        version = "2.1.0-alpha02"
+        version = "2.1.0-alpha03"
     )
 
     // POM metadata for the published artifact
