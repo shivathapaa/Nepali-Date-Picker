f2734fcdd0f5194ef49b903a5e47bf8447735008
Shiva Thapa <shivathapa0600@gmail.com>
2024-11-16 20:47:39 +0545

    Release of version 2.2.4 with compose multiplatform 1.7.1

--- nepali-date-picker/build.gradle.kts
+++ nepali-date-picker/build.gradle.kts
@@ -85,7 +85,7 @@ mavenPublishing {
     coordinates(
         groupId = "io.github.shivathapaa",
         artifactId = "nepali-date-picker",
-        version = "2.2.3"
+        version = "2.2.4"
     )
 
     // POM metadata for the published artifact
