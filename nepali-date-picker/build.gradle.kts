81d70d3f1998f45ee6de5ede639b2c70342c358e
Shiva Thapa <shivathapa0600@gmail.com>
2025-03-29 16:03:27 +0545

    [RELEASE]: Release of version 2.3.1 with dependency updates

--- nepali-date-picker/build.gradle.kts
+++ nepali-date-picker/build.gradle.kts
@@ -84,7 +84,7 @@ mavenPublishing {
     coordinates(
         groupId = "io.github.shivathapaa",
         artifactId = "nepali-date-picker",
-        version = "2.3.0"
+        version = "2.3.1"
     )
 
     configure(
